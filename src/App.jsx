cebd67136c617c2f6eea4d0e68c95c8bbdae27c3
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2024-07-02 21:17:30 +0530

    Add: Openanel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { Openpanel } from "@openpanel/web";
+
+const op = new Openpanel({
+  clientId: '8cf98031-325e-4c94-a996-630ad6c214cc',
+  trackScreenViews: true,
+  // trackAttributes: true,
+  // trackOutgoingLinks: true,
+});
 
 const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
